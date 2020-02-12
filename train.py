7479a55ecf226c174a2e43a2970b119073c96b50
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-02-12 20:54:54 +0000

    Train

--- train.py
+++ train.py
@@ -19,3 +19,64 @@ import torch.nn.init
 from torch.autograd import Variable
 
 from model import SegNet
+
+
+# Utils functions
+
+def get_random_pos(img, window_shape):
+    """ Extract of 2D random patch of shape window_shape in the image """
+    w, h = window_shape
+    W, H = img.shape[-2:]
+    x1 = random.randint(0, W - w - 1)
+    x2 = x1 + w
+    y1 = random.randint(0, H - h - 1)
+    y2 = y1 + h
+    return x1, x2, y1, y2
+
+def CrossEntropy2d(input, target, weight=None, size_average=True):
+    """ 2D version of the cross entropy loss """
+    dim = input.dim()
+    if dim == 2:
+        return F.cross_entropy(input, target, weight, size_average)
+    elif dim == 4:
+        output = input.view(input.size(0),input.size(1), -1)
+        output = torch.transpose(output,1,2).contiguous()
+        output = output.view(-1,output.size(2))
+        target = target.view(-1)
+        return F.cross_entropy(output, target,weight, size_average)
+    else:
+        raise ValueError('Expected 2 or 4 dimensions (got {})'.format(dim))
+
+def accuracy(input, target):
+    return 100 * float(np.count_nonzero(input == target)) / target.size
+
+def sliding_window(top, step=10, window_size=(20,20)):
+    """ Slide a window_shape window across the image with a stride of step """
+    for x in range(0, top.shape[0], step):
+        if x + window_size[0] > top.shape[0]:
+            x = top.shape[0] - window_size[0]
+        for y in range(0, top.shape[1], step):
+            if y + window_size[1] > top.shape[1]:
+                y = top.shape[1] - window_size[1]
+            yield x, y, window_size[0], window_size[1]
+            
+def count_sliding_window(top, step=10, window_size=(20,20)):
+    """ Count the number of windows in an image """
+    c = 0
+    for x in range(0, top.shape[0], step):
+        if x + window_size[0] > top.shape[0]:
+            x = top.shape[0] - window_size[0]
+        for y in range(0, top.shape[1], step):
+            if y + window_size[1] > top.shape[1]:
+                y = top.shape[1] - window_size[1]
+            c += 1
+    return c
+
+def grouper(n, iterable):
+    """ Browse an iterator by chunk of n elements """
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, n))
+        if not chunk:
+            return
+        yield chunk
