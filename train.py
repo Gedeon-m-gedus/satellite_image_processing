dc16ef401746b37de48c23a5770e14c8173c44bf
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-02-12 20:56:03 +0000

    Train

--- train.py
+++ train.py
@@ -80,3 +80,45 @@ def grouper(n, iterable):
         if not chunk:
             return
         yield chunk
+
+def metrics(predictions, gts, label_values=LABELS):
+    cm = confusion_matrix(
+            gts,
+            predictions,
+            range(len(label_values)))
+    
+    print("Confusion matrix :")
+    print(cm)
+    
+    print("---")
+    
+    # Compute global accuracy
+    total = sum(sum(cm))
+    accuracy = sum([cm[x][x] for x in range(len(cm))])
+    accuracy *= 100 / float(total)
+    print("{} pixels processed".format(total))
+    print("Total accuracy : {}%".format(accuracy))
+    
+    print("---")
+    
+    # Compute F1 score
+    F1Score = np.zeros(len(label_values))
+    for i in range(len(label_values)):
+        try:
+            F1Score[i] = 2. * cm[i,i] / (np.sum(cm[i,:]) + np.sum(cm[:,i]))
+        except:
+            # Ignore exception if there is no element in class i for test set
+            pass
+    print("F1Score :")
+    for l_id, score in enumerate(F1Score):
+        print("{}: {}".format(label_values[l_id], score))
+
+    print("---")
+        
+    # Compute kappa coefficient
+    total = np.sum(cm)
+    pa = np.trace(cm) / float(total)
+    pe = np.sum(np.sum(cm, axis=0) * np.sum(cm, axis=1)) / float(total*total)
+    kappa = (pa - pe) / (1 - pe);
+    print("Kappa: " + str(kappa))
+    return accuracy
