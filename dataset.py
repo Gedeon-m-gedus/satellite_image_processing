2081866691c3519d994909cee546e7f30379481d
Gedeon Mg <gedeonmuhawenayo@gmail.com>
2020-02-12 21:26:44 +0000

    Datasets

--- dataset.py
+++ dataset.py
@@ -1,3 +1,26 @@
+#importing all the required dependacies
+import numpy as np
+from skimage import io
+from glob import glob
+from tqdm import tqdm_notebook as tqdm
+from sklearn.metrics import confusion_matrix
+import random
+import itertools
+
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+import torch.optim.lr_scheduler
+import torch.nn.init
+from torch.autograd import Variable
+
+from model import SegNet
+
 # Dataset class
 
 class ISPRS_dataset(torch.utils.data.Dataset):
